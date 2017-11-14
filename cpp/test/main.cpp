f6355bd1bbeccc9de729ac7b18ef82c573113ce7
root <you@example.com>
2017-11-14 11:40:06 +0800

    1

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,64 +1,15 @@
 #include <iostream>
 using namespace std;
-class test2{
-public:
-	test2(int x):c(x){};
-	int c;
-};
-class test1{
-public:
-	test1(int p)
-	{}
-	test1(int p,char d)
-	{}
 
-	test1( )
-	{}
-// test1(){
-//
-//test2 tt(22);
-//
-// };
-//	test1():x(13)
-//	{}
-int x;
-//test2 ttt;
-};
-class test{
-
-public:
-	test():a(1),b('p'),ss(a),ppp(111)
-	{//
-//		test1 *t1;
-
-	//	t1=new test1();
-	//	cout<<t1.x<<endl;
-	}
-	int a;
-	char b;
-
-	 void fun() const
-	{
-			char c='o';
-
-				cout<<b<<endl;
-
-
-	}
-private:
-
-		typedef int index;
-		index t;
-		const int ppp;
-		int &ss;
-	 	test1 ttt;
-};
-
+#include "cpp-v4/12/screen5.h"
+#include "cpp-v4/12/screen3.h"
+using std::cout;
 int main()
 {
-
+	using namespace std;
+	Screen s(8,8);
+//	ostream os;
+	s.display(cout);
 	cout<<"test"<<endl;
-	test t;
-	//  test::index ss;
-	t.fun();
+	return 0;
 }
